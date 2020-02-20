b17f119b846a9f3f576bd7662fbea3121f69cf7b
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-20 19:19:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+#def key_for_min_value(name_hash)
+#  name_hash.each do |key|
+#    puts "#{key}"
+#end
+#name_hash[:adam]
+#end
+
+
 def key_for_min_value(name_hash)
-  name_hash.each do |key|
-    puts "#{key}"
-end
-name_hash[:adam]
+  name_hash.map do |name, num|
+  if num <= 1
+    return name
+  elsif num == 10
+   return name
+ else
+     puts name_hash
+   end
+  end
 end
